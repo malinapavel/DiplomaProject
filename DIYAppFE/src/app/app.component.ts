781f2c75289e9caa7bdd133a6bba77f84279b843
malinapavel <malina.pavel@student.upt.ro>
2024-03-13 19:40:28 +0200

    Testing FE+BE communication on dummy projects

--- DIYAppFE/src/app/app.component.ts
+++ DIYAppFE/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DIYAppFE';
+  public forecast?: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('https://localhost:7113/WeatherForecast').subscribe((result:WeatherForecast[]) => {
+      this.forecast = result;
+    });
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary?: string;
 }
